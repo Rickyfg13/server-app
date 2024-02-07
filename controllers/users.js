04342006046733fefa7711a405091fd540816e75
Ricky Farela Givano <ricky.givano@ecampus.ut.ac.id>
2024-02-07 10:29:33 +0700

    feat: adding top 5 user by location

--- controllers/users.js
+++ controllers/users.js
@@ -6,21 +6,35 @@ exports.getUserDetail = async (email) => {
       SELECT DISTINCT ON (d."Name")
       d."Name" ,d.gender ,d."Email",d."No Telp" , d."Age" AS tahun_lahir
       FROM dataset d
-      WHERE d."Email" = '${email}' --hallo sql injection
+      WHERE d."Email" = $1 --hallo sql injection
       GROUP BY 1,2,3,4,5;
-      ` // Menggunakan parameter langsung tanpa template strin
+      `,
+    [`${email}`]
   );
   return query;
 };
 
-exports.getUserDetail2 = async () => {
+exports.getTop5Loc = async (name) => {
   const query = db.query(
     `
-    SELECT DISTINCT ON (d."Name") 
-    d."Name" ,d."Number"
+    SELECT
+    "Name",
+    "Name of Location",
+    Frequency
+  FROM (
+    SELECT
+      "Name",
+      "Name of Location",
+      COUNT(*) AS Frequency,
+      ROW_NUMBER() OVER (PARTITION BY "Name of Location" ORDER BY COUNT(*) DESC) AS row_num
     FROM dataset d
-    WHERE d."Name of Location" ILIKE '%The Urban Cafe%'
-    GROUP BY 1,2`
+    WHERE "Name of Location" ilike  $1
+    GROUP BY "Name", "Name of Location"
+    ORDER BY frequency DESC 
+  ) AS UserFrequency
+  WHERE row_num <= 5;
+  `,
+    [`%${name}%`]
   );
   return query;
 };
--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const express = require("express");
 const response = require("../helpers/response");
 const users = express.Router();
 
-const { getUserDetail } = require("../controllers/users");
+const { getUserDetail, getTop5Loc } = require("../controllers/users");
 
 users.route("").get(async (req, res) => {
   try {
@@ -18,6 +18,17 @@ users.route("/detail-user").get(async (req, res) => {
     const data = req.query.email;
     if (data == undefined) response.error("Email is required");
     const result = await getUserDetail(data);
+    response.success(result, "data fetched!", res);
+  } catch (err) {
+    response.error({ error: err.message }, req.originalUrl, 403, res);
+  }
+});
+
+users.route("/top-5-by-location").get(async (req, res) => {
+  try {
+    const data = req.query.name;
+    if (data == undefined) response.error("Name is required");
+    const result = await getTop5Loc(data);
     response.success(result, "data fetched!", res);
   } catch (err) {
     response.error({ error: err.message }, req.originalUrl, 403, res);
